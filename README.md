bb0d0c3877ef2485badc969abc2673e98648f5fa
Luqmanul Hakim <luqmanulhakim.dev@gmail.com>
2021-12-13 14:48:08 +0700

    Inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# go-grpc
+
+compile proto: protoc --go_out=. *.proto
+run: go run main.go
+src: [Novalagung](https://dasarpemrogramangolang.novalagung.com/C-30-golang-grpc-protobuf.html)
---
+++ client/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"go-grpc/common/config"
+	model "go-grpc/common/model"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+)
+
+func serviceGarage() model.GaragesClient {
+	port := config.SERVICE_GARAGE_PORT
+	conn, err := grpc.Dial(port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("could not connect to", port, err)
+	}
+
+	return model.NewGaragesClient(conn)
+}
+
+func serviceUser() model.UsersClient {
+	port := config.SERVICE_USER_PORT
+	conn, err := grpc.Dial(port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("could not connect to", port, err)
+	}
+
+	return model.NewUsersClient(conn)
+}
+
+func main() {
+	user1 := model.User{
+		Id:       "n001",
+		Name:     "Noval Agung",
+		Password: "kw8d hl12/3m,a",
+		Gender:   model.UserGender(model.UserGender_value["MALE"]),
+	}
+
+	garage1 := model.Garage{
+		Id:   "q001",
+		Name: "Quel'thalas",
+		Coordinate: &model.GarageCoordinate{
+			Latitude:  45.123123123,
+			Longitude: 54.1231313123,
+		},
+	}
+
+	// ====== USER ======
+	user := serviceUser()
+
+	fmt.Println("\n", "===========> user test")
+
+	// register user1
+	user.Register(context.Background(), &user1)
+
+	// show all registered users
+	res1, err := user.List(context.Background(), new(empty.Empty))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	res1String, _ := json.Marshal(res1.List)
+	log.Println(string(res1String))
+
+	// ====== GARAGE ======
+	garage := serviceGarage()
+
+	fmt.Println("\n", "===========> garage test A")
+
+	// add garage1 to user1
+	garage.Add(context.Background(), &model.GarageAndUserId{
+		UserId: user1.Id,
+		Garage: &garage1,
+	})
+
+	// show all garages of user1
+	res2, err := garage.List(context.Background(), &model.GarageUserId{UserId: user1.Id})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	res2String, _ := json.Marshal(res2.List)
+	log.Println(string(res2String))
+}
